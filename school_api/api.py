2ad880fc51bab26e57979cfb340fb56471b6399f
JoMingyu <city7310@naver.com>
2017-10-02 22:42:18 +0900

    School-API for PIP done

--- school_api/api.py
+++ school_api/api.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 _url = 'http://{0}/sts_sci_md00_001.do?schulCode={1}&schulCrseScCode=4&schulKndScScore=04&schYm={2}{3:0>2}'
 
@@ -22,9 +23,61 @@ GYEONGNAM = 'stu.gne.go.kr'
 JEJU = 'stu.jje.go.kr'
 
 
-def parse(region, school_code, year, month, day=0):
-    pass
+class SchoolAPI:
+    def __init__(self, region, school_code):
+        self.region = region
+        self.school_code = school_code
+        self.menus = []
+        # 파싱되기 전 대기
+
+    def get_by_date(self, year, month, day):
+        if not self.menus:
+            self._parse(year, month)
+
+        return self.menus[day - 1]
+
+    def get_monthly(self, year, month):
+        if not self.menus:
+            self._parse(year, month)
+
+        return self.menus
+
+    def _parse(self, year, month):
+        resp = urlopen(_url.format(self.region, self.school_code, year, month))
+        soup = BeautifulSoup(resp, 'html.parser')
+        monthly_menu = [td.text for td in soup.find(class_='tbl_type3 tbl_calendar').find_all('td') if td.text != ' ']
+
+        for data in monthly_menu:
+            if len(data) > 1 and data != '자료가 없습니다':
+                day = int(re.findall('\d+', data)[0])
+                daily_menus = re.findall('[가-힇]+', data)
+
+                menu_dict = dict()
+                if '조식' in daily_menus:
+                    if '중식' in daily_menus:
+                        menu_dict['breakfast'] = daily_menus[1: daily_menus.index('중식')]
+                    elif '석식' in daily_menus:
+                        menu_dict['breakfast'] = daily_menus[1: daily_menus.index('석식')]
+                    else:
+                        menu_dict['breakfast'] = daily_menus[1:]
+                else:
+                    menu_dict['breakfast'] = '급식이 없습니다'
+
+                if '중식' in daily_menus:
+                    if '석식' in daily_menus:
+                        menu_dict['lunch'] = daily_menus[daily_menus.index('중식') + 1: daily_menus.index('석식')]
+                    else:
+                        menu_dict['lunch'] = daily_menus[daily_menus.index('중식') + 1:]
+                else:
+                    menu_dict['lunch'] = '급식이 없습니다'
+
+                if '석식' in daily_menus:
+                    menu_dict['dinner'] = daily_menus[daily_menus.index('석식') + 1:]
+                else:
+                    menu_dict['dinner'] = '급식이 없습니다'
+
+                self.menus.append({day: menu_dict})
 
 
 if __name__ == '__main__':
-    parse()
+    print(SchoolAPI(DAEJEON, 'G100000170').get_by_date(2016, 9, 20))
