8da75fa2b146160b6ee82fafd764d1cf9d2ce69b
JoMingyu <city7310@naver.com>
2017-10-02 21:40:16 +0900

    Initialize setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='school-api',
+      version='1.0',
+      url='https://github.com/JoMingyu/School-API',
+      license='MIT',
+      author='PlanB',
+      author_email='city7310@naver.com',
+      description='Korean School meal API',
+      install_requires=['bs4'])
